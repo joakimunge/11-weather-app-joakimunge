36e8ad5a8d0477a2a014dce7f66be2da6f2f81dd
Joakim Unge <ashba@unge.se>
2018-02-20 22:37:30 +0100

    Add default exports to index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//Exports
+export { default as Card } from './Card/Card.js';
+export { default as CardContainer } from './CardContainer/CardContainer.js';
+export { default as CardDay } from './CardDays/CardDays.js';
+export { default as CardDays } from './CardDay/CardDay.js';
+export { default as Header } from './Header/Header.js';
+
