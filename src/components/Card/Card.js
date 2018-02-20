2337391ea79f59c95a79ea5e5524eda19a75d932
Joakim Unge <ashba@unge.se>
2018-02-20 23:06:30 +0100

    Add react-transition-group

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CSSTransitionGroup } from 'react-transition-group' // ES6
 import { default as CardDays } from '../CardDays/CardDays';
 
 import './Card.css';
@@ -6,24 +7,31 @@ import './Card.css';
 class Card extends Component {
   render() {
     return (
-      <div className="card">
-        <div className={`card__weather card__weather--${this.props.color}`} >
-          <div className="card__location">
-            <h3 className="card__location__city">STOCKHOLM</h3>
-            <h4 className="card__location__city">CLEAR</h4>
+      <CSSTransitionGroup
+      transitionName="fade"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionEnter={false}
+      transitionLeave={false}>
+        <div className="card">
+          <div className={`card__weather card__weather--${this.props.color}`} >
+            <div className="card__location">
+              <h3 className="card__location__city">STOCKHOLM</h3>
+              <h4 className="card__location__city">CLEAR</h4>
+            </div>
+            <div className="card__temp">
+              <span>7°</span>
+            </div>
+            <div className="card__datetime">
+              <span className="card__time">08:54</span>
+              <span className="card__date">TUE, 13 JAN</span>
+            </div>
           </div>
-          <div className="card__temp">
-            <span>7°</span>
-          </div>
-          <div className="card__datetime">
-            <span className="card__time">08:54</span>
-            <span className="card__date">TUE, 13 JAN</span>
+          <div className="card__prognosis">
+            <CardDays />
           </div>
         </div>
-        <div className="card__prognosis">
-          <CardDays />
-        </div>
-      </div>
+      </CSSTransitionGroup>
     );
   }
 }
