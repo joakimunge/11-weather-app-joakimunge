03bcc91e8f3105ddc840aa5e61d727896cd05bd8
Joakim Unge <ashba@unge.se>
2018-02-21 16:03:42 +0100

    Add Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Loader.css';
+
+class Loader extends Component {
+  render() {
+    return (
+      <span>
+      	<i className="wi wi-day-sunny loader"></i>
+      </span>
+    );
+  }
+}
+
+export default Loader;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 //Exports
-export { default as Card } from './Card/Card.js';
-export { default as CardContainer } from './CardContainer/CardContainer.js';
-export { default as CardDay } from './CardDays/CardDays.js';
-export { default as CardDays } from './CardDay/CardDay.js';
 export { default as Header } from './Header/Header.js';
-
+export { default as CardContainer } from './CardContainer/CardContainer.js';
+export { default as Card } from './Card/Card.js';
+export { default as CardDays } from './CardDay/CardDay.js';
+export { default as CardDay } from './CardDays/CardDays.js';
+export { default as CardDetail } from './CardDetail/CardDetail.js';
+export { default as Loader } from './Loader/Loader.js';
