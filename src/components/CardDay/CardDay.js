979234544b57eb63f8fbfdf080f4baa314c9cd39
Joakim Unge <ashba@unge.se>
2018-02-21 16:04:58 +0100

    Pass state from parent Card to children

--- src/components/CardDay/CardDay.js
+++ src/components/CardDay/CardDay.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 import './CardDay.css';
 
 class CardDay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
+  }
+
+  epochToDay(epoch) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let date = new Date(epoch * 1000);
+    return days[date.getDay()];
+  }
+
   render() {
     return (
       <li className="CardDay">
       	<div className="day">
-      		<p>Thursday</p>
+      		<p>{this.epochToDay(this.state.forecast.time)}</p>
       	</div>
       	<div className="weather">
-      		<span className="temp">22°</span>
+      		<span className="temp">{this.state.forecast.temperatureMax.toFixed()}°</span>
       		<span className="icon"><i className="wi wi-night-sleet"></i></span>
       	</div>
       </li>
--- src/components/CardDays/CardDays.js
+++ src/components/CardDays/CardDays.js
@@ -4,12 +4,21 @@ import { default as CardDay } from '../CardDay/CardDay';
 import './CardDays.css';
 
 class CardDays extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = props;
+	}
+
+	componentDidMount() {
+		console.log(this.state);
+	}
   render() {
     return (
       <ul className="CardDays">
-      	<CardDay />
-      	<CardDay />
-      	<CardDay />
+      	<CardDay forecast={this.state.forecast.data[0]}/>
+      	<CardDay forecast={this.state.forecast.data[1]}/>
+      	<CardDay forecast={this.state.forecast.data[2]}/>
       </ul>
     );
   }
