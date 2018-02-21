c4131e137baa46c877134b08b3473d926dc34b3f
Joakim Unge <ashba@unge.se>
2018-02-21 22:31:46 +0100

    Update CardDetails component layout

--- src/components/CardDetail/CardDetail.js
+++ src/components/CardDetail/CardDetail.js
@@ -42,7 +42,54 @@ class CardDetail extends Component {
       transitionEnter={false}
       transitionLeave={false}>
         <div className="CardDetail">
-          
+          <div className="CardDetail__weather card__weather--night" >
+            <span class="CardDetail__backdrop"><i className="wi wi-night-sleet"></i></span>
+            <div className="CardDetail__location">
+              <h3>STOCKHOLM</h3>
+              <h4>THUNDER STORM</h4>
+            </div>
+            <div className="CardDetail__temp">
+              <span>7°</span>
+            </div>
+            <div className="CardDetail__datetime">
+              <span className="CardDetail__time">08:54</span>
+              <span className="CardDetail__date">TUE, 18 FEB</span>
+            </div>
+          </div>
+          <div className="CardDetail__prognosis">
+            <ul className="CardDetail__prognosis__list">
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+              <li className="CardDetail__prognosis__item">
+                <span className="icon"><i className="wi wi-night-sleet"></i></span>
+                <p>Thursday</p>
+                <span className="temp">22°</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </CSSTransitionGroup>
     );
