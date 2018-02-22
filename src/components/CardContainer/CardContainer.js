f86051d5655a46c54b29ef937ca29e7c99c44799
Joakim Unge <ashba@unge.se>
2018-02-22 22:09:51 +0100

    Refactor CardContainer component to render cards from db

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-
+import Card from '../Card/Card';
 import './CardContainer.css';
 
+import Database from '../../db.json'; // Temp database
+
 class CardContainer extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		console.log(Database);
+		console.log(this.props)
+	}
+
   render() {
     return (
       <div className="CardContainer">
-        {this.props.children}
+        { Database.locations.map(location => 
+        	<Card key={location.key} color="sunrise" city={location.name} />
+        	)
+      	}
       </div>
     );
   }
