8df457cc7178656441686f3bc9191ae5227ecf52
Joakim Unge <ashba@unge.se>
2018-02-22 22:10:07 +0100

    Add Helper component

---
+++ src/components/Helpers/Helpers.js
@@ -0,0 +1,36 @@
+// Convert epoch string to Day of the week
+export const EpochToDay = (epoch) => {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let date = new Date(epoch * 1000);
+    return days[date.getDay()];
+}
+
+// Convert timezone to location string
+export const CleanUpTimezone = (timezone) => {
+  let data = timezone.substring(timezone.lastIndexOf("/") + 1);
+  data = data.replace('_', ' ');
+  return data.toUpperCase();
+}
+
+// Convert time of day to night, day, sunrise, sunset
+export const TimeToString = (obj) => {
+  const periods = ['night', 'sunrise', 'day', 'sunset'];
+  let time = obj.getHours();
+  switch (time) {
+    case time >= 22:
+      return periods[0];
+    case time <= 7:
+      return periods[1];
+    case time >= 8:
+      return periods[2];
+    case time >= 18:
+      return periods[3];
+    default:
+      return periods[2]; 
+  }
+}
+
+// Convert Fahrenheit to Celcius and reverse
+export const UnitConversion = (degrees) => {
+  // Do stuff soon
+}
