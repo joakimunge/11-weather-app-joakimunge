9d9e96eeb4018b7352c2a3f2e2a14c2c0a1654c5
Joakim Unge <ashba@unge.se>
2018-02-22 22:10:54 +0100

    Add whimsical messages to loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -5,7 +5,8 @@ import './Loader.css';
 class Loader extends Component {
   render() {
     return (
-      <span>
+      <span className={`loader__container`}>
+      	{this.props.children}
       	<i className="wi wi-day-sunny loader"></i>
       </span>
     );
