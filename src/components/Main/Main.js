7c3a0154b6df603e2e4b466fda0080ca3b898388
Joakim Unge <ashba@unge.se>
2018-02-22 22:11:29 +0100

    Add Main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
+import { CardContainer, CardDetail } from '../';
+
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path="/" component={CardContainer} />
+          <Route path="/location/:location" component={CardDetail} details={true}/>
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default Main;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 //Exports
 export { default as Header } from './Header/Header.js';
+export { default as Main } from './Main/Main.js';
 export { default as CardContainer } from './CardContainer/CardContainer.js';
 export { default as Card } from './Card/Card.js';
 export { default as CardDays } from './CardDay/CardDay.js';
