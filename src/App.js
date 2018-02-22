4ba3b9e608100fedd5e46378f4a57a1e30620c39
Joakim Unge <ashba@unge.se>
2018-02-22 22:09:28 +0100

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardContainer, CardDetail, Header } from './components';
+import { Header, Main } from './components';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <CardContainer>
-          {/* <CardDetail color="sunrise" />
-          <CardDetail color="day" />
-          <CardDetail color="sunset" />
-          <CardDetail color="night" /> */}
-          <Card color="sunrise" city="Stockholm" />
-          <Card color="night" city="Dubai" />
-          <Card color="sunrise" city="Los Angeles" />
-          <Card color="sunset" city="One" />
-
-
-
-        </CardContainer>
+        <Main />
       </div>
     );
   }
