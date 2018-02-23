e5ccbfd5119509e0b6f007e5049a5c55e77425c5
Joakim Unge <ashba@unge.se>
2018-02-23 17:01:48 +0100

    Minor styling for app

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,10 +5,11 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
 
 import { default as CardDetail } from '../components/CardDetail/CardDetail.js';
 import { default as Loader } from '../components/Loader/Loader.js';
+import { default as Button } from '../components/Button/Button.js';
 import { default as Moment } from '../components/Helpers/Moment/Moment.js';
 
 
@@ -28,5 +29,5 @@ storiesOf('Moment', module).add('Normal', () => <Moment />);
 
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('Primary', () => <Button type="primary">Primary</Button>)
+  .add('Secondary', () => <Button type="secondary">Secondary</Button>);
