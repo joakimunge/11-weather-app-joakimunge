5f5c21ffe8640d1e2697169e75246388f8e92af2
Joakim Unge <ashba@unge.se>
2018-02-25 21:50:09 +0100

    Implement search method in Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 
 import './Search.css';
 
 class Search extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {value: ''}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+
+	}
+
+  handleChange(event) {
+    this.setState({value: event.target.value, submitted: false});
+  }
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.setState({
+			submitted: true
+		})
+	}
+
   render() {
     return (
-      <form>
-        <input className="search" type="text" name="search" placeholder="Find a location.."/>
-      </form>
+    	<div>
+	      <form onSubmit={this.handleSubmit}>
+	        <input className="search" value={this.state.value} onChange={this.handleChange} type="text" name="search" placeholder="Find a location.."/>
+	      </form>
+	      {this.state.submitted &&
+          <Redirect push to={{
+            pathname: '/location/' + this.state.value,
+          }}/>
+
+        }
+
+	     </div>
     );
   }
 }
