187f9de9db5e48b410c41ba72e3363afbd341f19
Joakim Unge <ashba@unge.se>
2018-02-25 21:50:30 +0100

    Add message if saved list is empty

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
 
-import Database from '../../db.json'; // Temp database
-
 class CardContainer extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      locations: JSON.parse(localStorage.getItem('savedLocations')) || []
+    }
+  }
 
   render() {
     return (
       <div className="CardContainer">
-        { Database.locations.map(location => 
-        	<Card key={location.key} color="sunrise" city={location.name} />
-        	)
-      	}
+        {this.state.locations.length 
+          ? this.state.locations.map((location, i) => <Card key={i} city={location.name} />) 
+          : <div className="CardContainer__message">
+              <h3>Looks like you don't have any saved locations yet.</h3>
+              <p>Try adding some by searching and saving them with the <i className="far fa-star"></i></p>
+            </div>
+        }
       </div>
     );
   }
